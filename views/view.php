2c6547d945fb8cb1b1dfbb93856699d5e2ff2226
gromovfjodor <gromovfjodor@ro.ru>
2019-09-25 15:12:40 +0300

    Update view.php

--- views/view.php
+++ views/view.php
@@ -5,6 +5,6 @@
  * Date: 14.06.2016
  * Time: 22:42
  */
-/* @var $widget gromomovfjodor\yandexMap\YandexMaps */
+/* @var $widget gromovfjodor\yandexMap\YandexMaps */
 ?>
 <div id="<?= $widget->id ?>" style="width: <?= $widget->windowWidth ?>; height: <?= $widget->windowHeight ?>;"></div>
