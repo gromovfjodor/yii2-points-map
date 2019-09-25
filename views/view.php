46424d9871b8a380753f85fc8c55730af5ac2ec4
gromovfjodor <gromovfjodor@ro.ru>
2019-09-25 15:12:24 +0300

    Update view.php

--- views/view.php
+++ views/view.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: mtcToolbox - http://mtcToolbox.com
+
  * Date: 14.06.2016
  * Time: 22:42
  */
-/* @var $widget mtcToolbox\yandexMap\YandexMaps */
+/* @var $widget gromomovfjodor\yandexMap\YandexMaps */
 ?>
 <div id="<?= $widget->id ?>" style="width: <?= $widget->windowWidth ?>; height: <?= $widget->windowHeight ?>;"></div>
